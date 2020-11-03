c1ebd72e5bda86d08a1078a77c516f640b60f9ba
dd395 <drichardssmith@my.wctc.edu>
2020-11-02 23:49:22 -0600

    Initial commit

---
+++ code.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("form").submit(enterName);
+
+
+    function enterName(event) {
+        //prevent form from flashing and resetting
+        event.preventDefault();
+
+        var rank = $("input[name=rank]:checked").val();
+        var firstName = $("#fname").val();
+        var lastName = $("#lname").val();
+        var greeting ="Hello," + rank + " " + firstName+ " " + lastName;
+
+        $("#output").text(greeting);
+
+
+    }
+});
