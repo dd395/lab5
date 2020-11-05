3c1ebfa58ddf67c71c983340c2e3c31a06ced001
dd395 <drichardssmith@my.wctc.edu>
2020-11-04 21:19:24 -0600

    Printing message. No color in message. Ask teacher for help.

--- code.js
+++ code.js
@@ -2,14 +2,23 @@ $(document).ready(function(){
     $("form").submit(enterName);
 
 
+
+
+
     function enterName(event) {
         //prevent form from flashing and resetting
         event.preventDefault();
-
         var rank = $("input[name=rank]:checked").val();
         var firstName = $("#fname").val();
         var lastName = $("#lname").val();
-        var greeting ="Hello," + rank + " " + firstName+ " " + lastName;
+
+        var greeting =`Hello, ${rank} ${firstName} ${lastName}`;
+
+
+
+
+
+
 
         $("#output").text(greeting);
 
